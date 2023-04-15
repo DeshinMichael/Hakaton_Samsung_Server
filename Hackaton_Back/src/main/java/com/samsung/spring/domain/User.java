cc9a1b04ef8d9dbed1fd9f8a249415e84a224faa
DeshinMichael <deshinmisha26@gmail.com>
2023-04-15 19:33:17 +0800

    add server

---
+++ Hackaton_Back/src/main/java/com/samsung/spring/domain/User.java
@@ -0,0 +1,41 @@
+package com.samsung.spring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "photoUrl")
+    private String photoUrl;
+
+    @Column(name = "progress")
+    private int progress;
+
+}
+
